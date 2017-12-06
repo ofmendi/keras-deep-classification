08a118d5cbdbaac1fd05ae5eec91e874186c0863
ofmendi <omerfarukmendi@gmail.com>
2017-12-06 23:31:54 +0300

    THE END

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/omerfaruk/Dropbox/keras-deep-classification"
+set -gx VIRTUAL_ENV "/home/omerfaruk/Desktop/Folders/keras-deep-classification"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
